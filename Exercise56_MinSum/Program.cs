14d5cc508b24f621519dea89a15b53cbed0da5a8
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-11-10 10:30:46 +0300

    Строка с наименьшей суммой в двумерном массиве

---
+++ Exercise56_MinSum/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 10, 100);
+PrintArray(array);
+WriteLine();
+Write($"Строка с наименьшей суммой элементов № {FindMinRowInArray(array)}");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int FindMinRowInArray (int [,] array){
+    int minRow = 0;
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum += array[0, j];
+    }
+    int minSum = sum;
+
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minSum) minRow = i; 
+    }
+    return minRow+1;
+}
